cab5ddf059d89866cf69584d676ba9080c86f685
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-06 19:22:04 -0500

    Update AST Node Types.cpp

--- AST Node Types.cpp
+++ AST Node Types.cpp
@@ -25,3 +25,35 @@ struct LoopNode : Node {
     std::unique_ptr<Node> condition;
     std::unique_ptr<Node> body;
 };
+
+struct Node {
+    virtual ~Node() = default;
+};
+
+struct FunctionNode : Node {
+    std::string name;
+    std::vector<std::unique_ptr<Node>> parameters;
+    std::unique_ptr<Node> body;
+};
+
+struct GaugeNode : Node {
+    std::string name;
+    float base;
+    float stretch;
+    bool adaptive;
+};
+
+struct ConditionalNode : Node {
+    std::unique_ptr<Node> condition;
+    std::unique_ptr<Node> thenBlock;
+    std::unique_ptr<Node> elseBlock;
+};
+
+struct LoopNode : Node {
+    std::unique_ptr<Node> condition;
+    std::unique_ptr<Node> body;
+};
+
+struct ErrorNode : Node {
+    std::string message;
+};
