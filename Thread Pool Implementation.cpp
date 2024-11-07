e3d34c824c05468c7b9ca5f61e65726adb92e07d
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-06 19:23:46 -0500

    Update Thread Pool Implementation.cpp

--- Thread Pool Implementation.cpp
+++ Thread Pool Implementation.cpp
@@ -58,3 +58,23 @@ private:
 
     void worker();
 };
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
+
+    template<class F>
+    void enqueue(F&& f);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    bool stop;
+
+    void worker();
+};
+
+// ThreadPool implementation (constructor, destructor, and worker logic)
