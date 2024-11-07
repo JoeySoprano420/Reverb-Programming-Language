3d789f42a15ec17abf76829ee2d084dd1b611486
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-06 19:06:41 -0500

    Update Creating The REPL.cpp

--- Creating The REPL.cpp
+++ Creating The REPL.cpp
@@ -129,3 +129,46 @@ void executeProgram(const ProgramNode& program) {
     // Existing execution code...
     tester.stopTiming();
 }
+#include <QApplication>
+#include <QMainWindow>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QString>
+
+class ReverbGUI : public QMainWindow {
+    Q_OBJECT
+
+public:
+    ReverbGUI() {
+        QWidget *centralWidget = new QWidget(this);
+        setCentralWidget(centralWidget);
+
+        QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+        textEdit = new QTextEdit(this);
+        QPushButton *executeButton = new QPushButton("Execute", this);
+
+        layout->addWidget(textEdit);
+        layout->addWidget(executeButton);
+
+        connect(executeButton, &QPushButton::clicked, this, &ReverbGUI::executeCommand);
+    }
+
+private slots:
+    void executeCommand() {
+        QString command = textEdit->toPlainText();
+        // Here we would execute the command using Reverb runtime logic
+        // For demonstration, just print the command
+        std::cout << "Executing: " << command.toStdString() << std::endl;
+    }
+
+private:
+    QTextEdit *textEdit;
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    ReverbGUI window;
+    window.show();
+    return app.exec();
+}
